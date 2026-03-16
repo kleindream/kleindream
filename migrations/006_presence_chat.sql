88fb443d82c47f0c4446cfc1e2101c049ccb9057
kleindream <rodrigokleinmc@hotmail.com>
2026-03-16 01:05:01 -0300

    Update 006_presence_chat.sql

--- migrations/006_presence_chat.sql
+++ migrations/006_presence_chat.sql
@@ -1,13 +1,12 @@
-
 CREATE TABLE IF NOT EXISTS presence (
-  user_id SERIAL PRIMARY KEY,
-  last_active INTEGER
+  user_id INTEGER PRIMARY KEY,
+  last_active BIGINT
 );
 
 CREATE TABLE IF NOT EXISTS private_messages (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id SERIAL PRIMARY KEY,
   from_user INTEGER,
   to_user INTEGER,
   message TEXT,
-  created_at INTEGER
+  created_at BIGINT
 );
