53ded154ad46a0788e69dbadcb2ddbc648324089
kleindream <rodrigokleinmc@hotmail.com>
2026-03-23 03:53:38 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const rateLimit = require("express-rate-limit");
 const { createClient } = require("@supabase/supabase-js");
 
 const { db, init, migrate, pool } = require("./db");
-const { getVibeFromBirthDate } = require("./utils/horoscopo.js");
+const { getVibeFromBirthDate } = require("./horoscopo");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
